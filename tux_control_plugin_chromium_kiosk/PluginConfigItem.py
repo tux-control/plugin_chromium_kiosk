385b9330d7d860bc86a7aba2014b907a90cc48e0
Adam Schubert <adam.schubert@sg1-game.net>
2022-07-26 14:34:08 +0200

    fix(Config): Fixes config parsing and generator

--- tux_control_plugin_chromium_kiosk/PluginConfigItem.py
+++ tux_control_plugin_chromium_kiosk/PluginConfigItem.py
@@ -12,51 +12,14 @@ class PluginConfigItem(IPluginConfigItem):
 
     def __init__(self,
                  name: str,
-                 full_screen: bool,
-                 touchscreen: bool,
-                 home_page: bool,
-                 idle_time: int,
-                 white_list_enabled: bool,
-                 white_list_urls: List[str],
-                 white_list_iframe_enabled: List[str],
-                 nav_bar_enabled: bool,
-                 nav_bar_enabled_buttons: List[str],
-                 nav_bar_vertical_position: str,
-                 nav_bar_horizontal_position: str,
-                 nav_bar_width: int,
-                 virtual_keyboard_enabled: bool,
-                 screen_saver_enabled: bool,
-                 screen_saver_idle_time: bool,
-                 screen_saver_text: str,
-                 screen_rotation: str,
-                 touchscreen_rotation: str,
                  plugin_config_options: List[PluginConfigOption]
                  ):
         self.name = name
-        self.full_screen = full_screen
-        self.touchscreen = touchscreen
-        self.home_page = home_page
-        self.idle_time = idle_time
-        self.white_list_enabled = white_list_enabled
-        self.white_list_urls = white_list_urls
-        self.white_list_iframe_enabled = white_list_iframe_enabled
-        self.nav_bar_enabled = nav_bar_enabled
-        self.nav_bar_enabled_buttons = nav_bar_enabled_buttons
-        self.nav_bar_vertical_position = nav_bar_vertical_position
-        self.nav_bar_horizontal_position = nav_bar_horizontal_position
-        self.nav_bar_width = nav_bar_width
-        self.virtual_keyboard_enabled = virtual_keyboard_enabled
-        self.screen_saver_enabled = screen_saver_enabled
-        self.screen_saver_idle_time = screen_saver_idle_time
-        self.screen_saver_text = screen_saver_text
-        self.screen_rotation = screen_rotation
-        self.touchscreen_rotation = touchscreen_rotation
         self.plugin_config_options = plugin_config_options
 
     @staticmethod
     def from_chromium_kiosk_config(chromium_kiosk_config: dict, plugin_config_options: List[PluginConfigOption]) -> 'PluginConfigItem':
-        return PluginConfigItem(
-            name='Main config',
+        data = dict(
             full_screen=chromium_kiosk_config.get('FULL_SCREEN', True),
             touchscreen=chromium_kiosk_config.get('TOUCHSCREEN', True),
             home_page=chromium_kiosk_config.get('HOME_PAGE', 'https://salamek.cz'),
@@ -79,45 +42,62 @@ class PluginConfigItem(IPluginConfigItem):
             screen_saver_text=chromium_kiosk_config.get('SCREEN_SAVER', {}).get('TEXT', False),
 
             screen_rotation=chromium_kiosk_config.get('SCREEN_ROTATION', 'normal'),
-            touchscreen_rotation=chromium_kiosk_config.get('TOUCHSCREEN_ROTATION', 'normal'),
+            touchscreen_rotation=chromium_kiosk_config.get('TOUCHSCREEN_ROTATION', 'normal')
+        )
+
+        for plugin_config_option in plugin_config_options:
+            plugin_config_option._value = data.get(plugin_config_option.key)
+
+        return PluginConfigItem(
+            name='Main config',
             plugin_config_options=plugin_config_options
         )
 
     @staticmethod
     def from_dict(data: dict) -> 'PluginConfigItem':
-        del data['key']
-        del data['is_deletable']
-        del data['is_editable']
-        return PluginConfigItem(**data)
+        plugin_config_options = []
+        for item in data.get('plugin_config_options'):
+            plugin_config_option = PluginConfigOption.from_dict(item)
+            plugin_config_options.append(plugin_config_option)
+
+        name = data.get('name')
+        return PluginConfigItem(
+            name=name,
+            plugin_config_options=plugin_config_options,
+        )
 
     def to_chromium_kiosk_config(self) -> dict:
+        data = {}
+        for plugin_config_option in self.plugin_config_options:
+            data[plugin_config_option.key] = plugin_config_option.value
+
         return {
-            'FULL_SCREEN': self.full_screen,
-            'TOUCHSCREEN': self.touchscreen,
-            'HOME_PAGE': self.home_page,
-            'IDLE_TIME': self.idle_time,
+            'FULL_SCREEN': data.get('full_screen'),
+            'TOUCHSCREEN': data.get('touchscreen'),
+            'HOME_PAGE': data.get('home_page'),
+            'IDLE_TIME':  data.get('idle_time'),
             'WHITE_LIST': {
-                'ENABLED': self.white_list_enabled,
-                'URLS': self.white_list_urls,
-                'IFRAME_ENABLED': self.white_list_iframe_enabled,
+                'ENABLED': data.get('white_list_enabled'),
+                'URLS': data.get('white_list_urls'),
+                'IFRAME_ENABLED': data.get('white_list_iframe_enabled'),
             },
             'NAV_BAR': {
-                'ENABLED': self.nav_bar_enabled,
-                'ENABLED_BUTTONS': self.nav_bar_enabled_buttons,
-                'VERTICAL_POSITION': self.nav_bar_vertical_position,
-                'HORIZONTAL_POSITION': self.nav_bar_horizontal_position,
-                'WIDTH': self.nav_bar_width,
+                'ENABLED': data.get('nav_bar_enabled'),
+                'ENABLED_BUTTONS': data.get('nav_bar_enabled_buttons'),
+                'VERTICAL_POSITION': data.get('nav_bar_vertical_position'),
+                'HORIZONTAL_POSITION': data.get('nav_bar_horizontal_position'),
+                'WIDTH': data.get('nav_bar_width'),
             },
             'VIRTUAL_KEYBOARD': {
-                'ENABLED': self.virtual_keyboard_enabled
+                'ENABLED': data.get('virtual_keyboard_enabled')
             },
             'SCREEN_SAVER': {
-                'ENABLED': self.screen_saver_enabled,
-                'IDLE_TIME': self.screen_saver_idle_time,
-                'TEXT': self.screen_saver_text,
+                'ENABLED': data.get('screen_saver_enabled'),
+                'IDLE_TIME': data.get('screen_saver_idle_time'),
+                'TEXT': data.get('screen_saver_text'),
             },
-            'SCREEN_ROTATION': self.screen_rotation,
-            'TOUCHSCREEN_ROTATION': self.touchscreen_rotation,
+            'SCREEN_ROTATION': data.get('screen_rotation'),
+            'TOUCHSCREEN_ROTATION': data.get('touchscreen_rotation'),
         }
 
     def to_dict(self) -> dict:
@@ -126,24 +106,6 @@ class PluginConfigItem(IPluginConfigItem):
             'key': self.key,
             'is_deletable': self.is_deletable,
             'is_editable': self.is_editable,
-            'plugin_config_options': self.plugin_config_options,
-            'full_screen': self.full_screen,
-            'touchscreen': self.touchscreen,
-            'home_page': self.home_page,
-            'idle_time': self.idle_time,
-            'white_list_enabled': self.white_list_enabled,
-            'white_list_urls': self.white_list_urls,
-            'white_list_iframe_enabled': self.white_list_iframe_enabled,
-            'nav_bar_enabled': self.nav_bar_enabled,
-            'nav_bar_enabled_buttons': self.nav_bar_enabled_buttons,
-            'nav_bar_vertical_position': self.nav_bar_vertical_position,
-            'nav_bar_horizontal_position': self.nav_bar_horizontal_position,
-            'nav_bar_width': self.nav_bar_width,
-            'virtual_keyboard_enabled': self.virtual_keyboard_enabled,
-            'screen_saver_enabled': self.screen_saver_enabled,
-            'screen_saver_idle_time': self.screen_saver_idle_time,
-            'screen_saver_text': self.screen_saver_text,
-            'screen_rotation': self.screen_rotation,
-            'touchscreen_rotation': self.touchscreen_rotation,
+            'plugin_config_options': self.plugin_config_options
         }
 
